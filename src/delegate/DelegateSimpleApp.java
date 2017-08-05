19184cc6171b1df91f39eff7b138aa09b7143886
AndriiAV <Andrii.Andruschenko@gmail.com>
2017-08-05 13:15:55 +0300

    Pattern Delegate (simple example)

---
+++ src/delegate/DelegateSimpleApp.java
@@ -0,0 +1,26 @@
+package delegate;
+
+public class DelegateSimpleApp {
+
+	public static void main(String[] args) {
+		A a = new A();
+		a.f();
+		
+		B b = new B();
+		b.f();
+	}
+
+}
+
+class A{
+	void f() {
+		System.out.println("f()");
+	}
+}
+
+class B{
+	A a = new A();
+	void f() {
+	a.f();
+	}
+}
